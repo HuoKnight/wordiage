5c4da50561caf7127eddcbedfca65c7c42f02b72
HuoKnight <noman.gronckle@gmail.com>
2022-06-16 12:57:50 -0700

    commit whatever is here to wipe machine

--- src/modules/define.jl
+++ src/modules/define.jl
@@ -1,5 +1,7 @@
 function define(word, isFile)
+	str = ""
 	obj = JSON3.read(HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/$word").body)[1]
 	
-	println("\"word\": ", obj.phonetics)
+	println(obj["phonetic"])
+
 end
